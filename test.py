f1d9b5b1f074e2490557a5e7e10c0e882f6f7aca
stone <github@stone-bo.com>
2024-12-31 21:32:35 +0000

    chore: remove obsolete test.py file to clean up the codebase

--- test.py
+++
@@ -1,8 +0,0 @@
-from ures.markdown import Zettelkasten
-
-
-if __name__ == "__main__":
-    zettel = Zettelkasten(None, "type", "url", ["tags"], ["aliases"])
-    # zettel.add_content("New Note")
-    # zettel.add_tag("new-tag")
-    # zettel.save("1.md")
