a375e0641bc5aeb01952cb9917b2d1a7daa8324d
stonebo <github@stone-bo.com>
2025-03-07 20:37:44 +0000

    feat(docker): add a new class in docker for cleaning container and images (#15)
    All containers and images are shown below are removed automatically
    - dangling images
    - stopped container
    - exited container

--- ures/docker/__init__.py
+++ ures/docker/__init__.py
@@ -2,9 +2,11 @@ from .conf import BuildConfig, RuntimeConfig
 from .container import Container
 from .containers import Containers
 from .image import Image, ImageOrchestrator
+from .cleanup import DockerCleanup
 
 
 __all__ = [
+    "DockerCleanup",
     "BuildConfig",
     "RuntimeConfig",
     "Image",
